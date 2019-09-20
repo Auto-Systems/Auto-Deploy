b1bf8e13b5fb6a98011a5c35e81c71dbea187f8c
Kristian F Jones <me@kristianjones.xyz>
2019-09-19 21:45:59 -0500

    feat: Stuff

--- API/src/Modules/Controllers/Nodes/NodeOS.ts
+++ API/src/Modules/Controllers/Nodes/NodeOS.ts
@@ -1,9 +1,4 @@
 // API/src/Modules/Controllers/Nodes/NodeOS.ts
 import { registerEnumType } from 'type-graphql'
 
-export enum NodeOS {
-  UBUNTU = 'UBUNTU',
-  DEBIAN = 'DEBIAN'
-}
-
 registerEnumType(NodeOS, { name: 'NodeOS' })
