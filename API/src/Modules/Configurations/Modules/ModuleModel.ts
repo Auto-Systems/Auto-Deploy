6f3e441d717d5ce1aeb15087c834d76307771b2d
Kristian F Jones <me@kristianjones.xyz>
2019-09-22 15:45:53 -0500

    fix: Minor fix

--- API/src/Modules/Configurations/Modules/ModuleModel.ts
+++ API/src/Modules/Configurations/Modules/ModuleModel.ts
@@ -69,6 +69,9 @@ export class Module extends BaseEntity {
         { key: 'GIT_URL', value: git },
         { key: 'TYPE', value: moduleType! },
       ],
+      hostConfig: {
+        VolumesFrom: ['autodeploy']
+      }
     });
   }
 
